42c8d0055ac7155b058152e82b1280a6ccbf1314
Manik <manik@pixelconception.com>
2024-07-05 16:52:49 +0530

    Final code for TCC08

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-var students = [
-    {
-        name: "John",
-        score: 70,
-    },
-    {
-        name: "Mark",
-        score: 90,
-    },
-];
-for (var _i = 0, students_1 = students; _i < students_1.length; _i++) {
-    var student = students_1[_i];
-    console.log("Name:", student.name);
-    console.log("Name:", student.grade);
+// Function to add two number
+function addNumbers(a, b) {
+  return a + b;
 }
+
+console.log(addNumbers(2, 3));
+console.log(addNumbers("text", 3));
