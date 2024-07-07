1331aaf729db2d950f24c263096775bed1b23d5f
Manik <manik@pixelconception.com>
2024-07-07 16:46:20 +0530

    Final code for TCC50

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 "use strict";
-// Types can be casted other types.
-// This shoudl be done when you are sure that TypeScript is not able to infer the types correctly
-let firstName = "Mark";
-let lastName = "Doe";
+let city = "New York"; // string
+let population = 8400000;
+const age = 32;
+let oldAge = 79;
+let newAge = oldAge;
+let data = new Map();
+let score = [90, 86, 100];
+/**
+ * Are the following statements valid
+ * Check if below lines of code are valid as per TypeScript or not without uncommenting them
+ *  */
+// age = 85;
+// score.push(10);
+// let customAge: CustomAge = 50;
+// let primitive: Primitive = new Date();
+// let years: CheckScore = [];
--- index.ts
+++ index.ts
@@ -1,4 +1,23 @@
-// Types can be casted other types.
-// This shoudl be done when you are sure that TypeScript is not able to infer the types correctly
-let firstName = <any>"Mark";
-let lastName = "Doe" as any;
+let city = "New York"; // string
+let population = 8400000;
+const age = 32;
+let oldAge = 79 as const;
+let newAge = oldAge;
+let data = new Map();
+let score = [90, 86, 100];
+type Primitive = string | number | boolean;
+type CustomName = "John" extends string ? string : "John";
+type CustomAge = typeof newAge extends number ? 79 : number;
+type CheckData = typeof data extends Object ? true : false;
+type CheckScore = typeof score extends never ? {} : [];
+
+/**
+ * Are the following statements valid
+ * Check if below lines of code are valid as per TypeScript or not without uncommenting them
+ *  */
+
+// age = 85;
+// score.push(10);
+// let customAge: CustomAge = 50;
+// let primitive: Primitive = new Date();
+// let years: CheckScore = [];
