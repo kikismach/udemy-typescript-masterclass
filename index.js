a4df9954bbd80f6c8458178c0b903839f3425f1e
Manik <manik@pixelconception.com>
2024-07-07 17:42:19 +0530

    Final code for TCC64

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 "use strict";
-// A post can be strictly typed using type annotations
-// let post: {
+// One of having nested objects
+// type Post = {
 //   title: string; // Type Annotations using semi-colons instead of commas
 //   content: string;
 //   date: Date;
-// } = {
-//   title: "This is a blog post",
-//   content: "Content of the post",
-//   date: new Date(),
+//   author: {
+//     name: string;
+//     age: number;
+//     email: string;
+//   };
 // };
 let post = {
     title: "This is a blog post",
     content: "Content of the post",
     date: new Date(),
-};
-// Advantage of type declaration, can use the same type again for a new object
-let post2 = {
-    title: "This is a blog post 2",
-    content: "Content of the post 2",
-    date: new Date(),
+    author: {
+        name: "John",
+        age: 32,
+        email: "john@doe.com",
+    },
 };
--- index.ts
+++ index.ts
@@ -1,31 +1,36 @@
-// A post can be strictly typed using type annotations
-// let post: {
+// One of having nested objects
+// type Post = {
 //   title: string; // Type Annotations using semi-colons instead of commas
 //   content: string;
 //   date: Date;
-// } = {
-//   title: "This is a blog post",
-//   content: "Content of the post",
-//   date: new Date(),
+//   author: {
+//     name: string;
+//     age: number;
+//     email: string;
+//   };
 // };
 
-// We can use Type declarations using Type Alias
+// Better to declare a separate type for Author
+type Author = {
+  name: string;
+  age: number;
+  email: string;
+};
 
 type Post = {
   title: string; // Type Annotations using semi-colons instead of commas
   content: string;
   date: Date;
+  author: Author; // Assign Author type to author property on Post type
 };
 
 let post: Post = {
   title: "This is a blog post",
   content: "Content of the post",
   date: new Date(),
-};
-
-// Advantage of type declaration, can use the same type again for a new object
-let post2: Post = {
-  title: "This is a blog post 2",
-  content: "Content of the post 2",
-  date: new Date(),
+  author: {
+    name: "John",
+    age: 32,
+    email: "john@doe.com",
+  },
 };
