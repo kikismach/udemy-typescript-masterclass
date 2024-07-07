2f23eb67d6fd0616775946478548a8eafc780192
Manik <manik@pixelconception.com>
2024-07-07 11:58:20 +0530

    Final code for TCC42

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-/**
- * Assign the correct types to each of the following variables
- */
-let message: string = "Hello, TypeScript!";
-let age: number = 42;
-let isStudent: boolean = true;
-let fetched: null = null;
-let user: undefined = undefined;
-let largeNumber: bigint = 9007199254740991n;
-let unique: symbol = Symbol("uniqueSymbol");
+// any type is the default JavaScript behavior of types.
+// The intention is to avoid any type
+let firstName: any = "Mark";
+
+// Once any time is assigned then any value can be assigned to such variable
+firstName = 123;
+
+// Here we are assigning an array
+firstName = [5, 4, 8, 4];
