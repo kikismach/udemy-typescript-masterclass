8b7399542a51e3eb9ebce5b12172fad800733abc
Manik <manik@pixelconception.com>
2024-07-07 17:31:13 +0530

    Final code for TCC63

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 "use strict";
-let city = "New York"; // string
-let population = 8400000; // number
-const age = 32; // litral 32
-let oldAge = 79; // litral 79
-let newAge = oldAge; // litral 79
-let data = new Map(); // Map<any, any>
-let score = [90, 86, 100]; // number []
-/**
- * Are the following statements valid
- * Check if below lines of code are valid as per TypeScript or not without uncommenting them
- *  */
-age = 85; // @Error age is a const and connot be re-assigned
-score.push(10); // This is valid as score is an array of numbers
-score.push("New Score"); // @Errro Score is an array of number and can only have numbers
-let customAge = 50; // @Error custom age has a literal value of 79
-let primitive = new Date(); // @Error data is not a part of the union
-let years = []; // Valid is years is a litral empty array
+// A post can be strictly typed using type annotations
+// let post: {
+//   title: string; // Type Annotations using semi-colons instead of commas
+//   content: string;
+//   date: Date;
+// } = {
+//   title: "This is a blog post",
+//   content: "Content of the post",
+//   date: new Date(),
+// };
+let post = {
+    title: "This is a blog post",
+    content: "Content of the post",
+    date: new Date(),
+};
+let post2 = {
+    title: "This is a blog post 2",
+    content: "Content of the post 2",
+    date: new Date(),
+};
--- index.ts
+++ index.ts
@@ -1,29 +1,30 @@
-let person = {
-  name: "Mark",
-  age: 32,
+// A post can be strictly typed using type annotations
+// let post: {
+//   title: string; // Type Annotations using semi-colons instead of commas
+//   content: string;
+//   date: Date;
+// } = {
+//   title: "This is a blog post",
+//   content: "Content of the post",
+//   date: new Date(),
+// };
+
+// We can use Type declarations using Type Alias
+
+type Post = {
+  title: string; // Type Annotations using semi-colons instead of commas
+  content: string;
+  date: Date;
 };
 
-// Car is any object without a shape or defined properties
-let car: Object = {
-  brand: "BMW",
-  color: "black",
+let post: Post = {
+  title: "This is a blog post",
+  content: "Content of the post",
+  date: new Date(),
 };
 
-// The problem using teh Object type, its a super type and Arrays are also objects
-let score: Object = [];
-
-// If you see just because car is a Object I can re-assign it as an array
-// ! This is a problem
-car = [21, 32, 48];
-
-// Defining an object with the litral syntax
-let newCar: {} = {
-  brand: "BMW",
-  color: "black",
-};
-
-// The problem with usign the litral object {} is that now properties are not defined and can vary
-// This can lead to errors in the application
-newCar = {
-  brand: "BMW",
+let post2: Post = {
+  title: "This is a blog post 2",
+  content: "Content of the post 2",
+  date: new Date(),
 };
