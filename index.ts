4c8ea15030e7a3935e2d2106f84bd0f35e870deb
Manik <manik@pixelconception.com>
2024-07-08 10:11:22 +0530

    Final code for TCC82

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
+// Declaring an array of numbers
+let a: number[] = [1, 2, 3];
+//  Generic syntax of declaring array of strings
+let b: Array<string> = ["a", "b", "c"];
+// Array containing mutiple types
+let c: (string | number)[] = ["a", "b", 1];
+
 // Declare a Caterer type
 type Caterer = {
   name: string;
@@ -20,20 +27,26 @@ type Airplane = {
   seats: Seats;
 };
 
+// Declarign a type for an array of Airplanes
+// Example of an array of objects
+type Airplanes = Airplane[];
+
 // Assign Airplane Type to Object
-let airplane: Airplane = {
-  model: "Airbus A380",
-  flightNumber: "A2201",
-  timeOfDeparture: new Date(),
-  timeOfArrival: new Date(),
-  caterer: {
-    name: "Special Food Ltd",
-    address: "484, Some Street, New York",
-    phone: 7867856751,
+let airplanes: Airplanes = [
+  {
+    model: "Airbus A380",
+    flightNumber: "A2201",
+    timeOfDeparture: new Date(),
+    timeOfArrival: new Date(),
+    caterer: {
+      name: "Special Food Ltd",
+      address: "484, Some Street, New York",
+      phone: 7867856751,
+    },
+    seats: {
+      A1: "John Doe",
+      A2: "Mark Doe",
+      A3: "Sam Doe",
+    },
   },
-  seats: {
-    A1: "John Doe",
-    A2: "Mark Doe",
-    A3: "Sam Doe",
-  },
-};
+];
