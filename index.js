e81b4fc538fb03983ef2ca0db0cb2dff63cb9c1f
Manik <manik@pixelconception.com>
2024-07-08 19:01:26 +0530

    updating code on the branch

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 "use strict";
-let id = Symbol(1234);
-let alphabeticId = Symbol("id");
-let user = {
-    [id]: "12344",
-    [alphabeticId]: "uniqueId",
-    name: "Mark",
-    getId() {
-        return this[id];
-    },
-};
-// Symbols are  private
-console.log(user.name);
-// will throw a typescript error with id not accessible
-// Throws error because the key here is not 'id' but a symbol generated with 1234 who's value is unknown but guaranteed to be unique
-//! console.log(user.id);
-// The id property does exist add the getId method to the object
-console.log(user.getId());
+// any type is the default JavaScript behavior of types.
+// The intention is to avoid any type
+let firstName = "Mark";
+// Once any time is assigned then any value can be assigned to such variable
+firstName = 123;
+// Here we are assigning an array
+firstName = [5, 4, 8, 4];
--- index.ts
+++ index.ts
@@ -7,3 +7,14 @@ firstName = 123;
 
 // Here we are assigning an array
 firstName = [5, 4, 8, 4];
+
+// TypeScript warns us if any value is implicitly any
+// Which means we did not defione a type and TS knows that the value can be anything
+// so it warns us that we should assigna strict type to it
+
+function returnParam(param) {
+  return param;
+}
+
+//There is an option to diable this behaviour by setting noImplicitAny: false;
+// but we do not want to do this
