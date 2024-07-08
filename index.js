fda56ff8136c7d011b7cabba0fc890c116510311
Manik <manik@pixelconception.com>
2024-07-08 18:06:34 +0530

    Final code for TCC112

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ const part2 = str.slice(7, 10);
 // based on the arguments passed to the function
 console.log(part1);
 console.log(part2);
+const reserve = (departureDate, returnDateOrDepartingFrom, departingFromOrDestination, destination) => {
+    if (returnDateOrDepartingFrom instanceof Date && destination) {
+        return {
+            departureDate: departureDate,
+            returnDate: returnDateOrDepartingFrom,
+            departingFrom: departingFromOrDestination,
+            destination: destination,
+        };
+    }
+    else if (typeof returnDateOrDepartingFrom === "string") {
+        return {
+            departureDate: departureDate,
+            departingFrom: returnDateOrDepartingFrom,
+            destination: departingFromOrDestination,
+        };
+    }
+    throw new Error("Please provide valid details to reserve a ticket");
+};
+// Try making a reservation
+console.log(reserve(new Date(), new Date(), "New York", "Washington"));
+console.log(reserve(new Date(), "New York", "Washington"));
--- index.ts
+++ index.ts
@@ -31,5 +31,35 @@ type Reservation = {
 // Using a more explicit syntax for call signatures
 // This is considered as an object with keys as function params
 type Reserve = {
-  (departureDate: Date, returnDate: Date, departingFrom: string, destination: string): Reservation;
+  (departureDate: Date, returnDate: Date, departingFrom: string, destination: string):
+    | Reservation
+    | never;
+  (departureDate: Date, departingFrom: string, destination: string): Reservation | never;
 };
+
+const reserve: Reserve = (
+  departureDate: Date,
+  returnDateOrDepartingFrom: Date | string,
+  departingFromOrDestination: string,
+  destination?: string
+) => {
+  if (returnDateOrDepartingFrom instanceof Date && destination) {
+    return {
+      departureDate: departureDate,
+      returnDate: returnDateOrDepartingFrom,
+      departingFrom: departingFromOrDestination,
+      destination: destination,
+    };
+  } else if (typeof returnDateOrDepartingFrom === "string") {
+    return {
+      departureDate: departureDate,
+      departingFrom: returnDateOrDepartingFrom,
+      destination: departingFromOrDestination,
+    };
+  }
+  throw new Error("Please provide valid details to reserve a ticket");
+};
+
+// Try making a reservation
+console.log(reserve(new Date(), new Date(), "New York", "Washington"));
+console.log(reserve(new Date(), "New York", "Washington"));
