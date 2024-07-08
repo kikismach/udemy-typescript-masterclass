31a59f28dbe8766e16af586350523b0f968c9625
Manik <manik@pixelconception.com>
2024-07-08 17:29:14 +0530

    Final code for TCC109

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 "use strict";
-//  Async fucntion in JavaScript always return a promise
-// Declaration of async function using the function keyword
-async function fetchFromDatabase(id) { }
-//  Declaration of async fucntion using the arrow function syntax
-const anotherAsyncFunction = async () => { };
-// Async fucntion as a function express
-const fucntionExpression = async function () { };
-// Setting return types apart from any
-async function returnString(id) {
-    return Promise.resolve("string");
+// Unlimited function params using the
+function multiplyBy(by, ...numbers) {
+    return numbers.map((eachNumber) => by * eachNumber);
 }
-// If User type is nto returned TS throws an error
-async function returnObject(id) {
-    return Promise.resolve({ name: "John", age: 21 });
-}
+// Calling the function
+console.log(multiplyBy(2, 3, 4, 5));
+console.log(multiplyBy(2, 3, 4));
+// Strictly typing rest parameters
+const args = [8, 5];
+// We get this erro because if we see the signature of the function it takes in 2 arguments
+// Here TS s not sure that args will always contain 2 arguments
+const angle = Math.atan2(...args);
+// The solution is to define args as a tuple
+const args1 = [8, 5];
+// Now TypeScript will not thwo an error because it will now know that args are always of a fixed length because its a tuple
+const angle1 = Math.atan2(...args1);
+// We can define type annotate it like this as well
+const args2 = [8, 5]; // Here using as const is not needed
+const angle2 = Math.atan2(...args2);
