8a676082499d55d723a16194c9b3a34ad9d2426a
Manik <manik@pixelconception.com>
2024-07-08 15:00:35 +0530

    Final Code for TCC102

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 "use strict";
-// We have an enume for shapes
-var ShapeKind;
-(function (ShapeKind) {
-    ShapeKind["Circle"] = "circle";
-    ShapeKind["Square"] = "sqaure";
-})(ShapeKind || (ShapeKind = {}));
-// Now enuma act as types as TS is able to identify that square enum cannot be assigned to circle type
-let circle = {
-    kind: ShapeKind.Square,
-    radius: 100,
-};
-//  Enums automatically become union of each of its members
-// Here the ShapeKind Enum is acting as a union of ""
-function printShape(shape /* same as "Circle"| "Square"  */) {
-    // We can check teh shape in step one
-    console.log(`Shape is: ${shape}`);
-    // Also an or comparison TS will say that this is already an enum so this comparison is not needed
-    if (ShapeKind.Circle || ShapeKind.Square) {
-    }
+// Defining a named function in TypeScript
+function intro(name, age) {
+    return `My name is ${name} and I am ${age} years old`;
 }
-// We can invoke the function
-printShape(ShapeKind.Circle);
+// Using a fucntion expression
+const intro2 = function (name, age) {
+    return `My name is ${name} and I am ${age} years old`;
+};
+// Using the arrow function syntax
+const intro3 = (name, age) => {
+    return `My name is ${name} and I am ${age} years old`;
+};
--- index.ts
+++ index.ts
@@ -1,36 +1,14 @@
-// We have an enume for shapes
-enum ShapeKind {
-  Circle = "circle",
-  Square = "sqaure",
+// Defining a named function in TypeScript
+function intro(name: string, age: number): string {
+  return `My name is ${name} and I am ${age} years old`;
 }
 
-// A circle type would have additional properties
-type Circle = {
-  kind: ShapeKind.Circle;
-  radius: number;
+// Using a fucntion expression
+const intro2 = function (name: string, age: number): string {
+  return `My name is ${name} and I am ${age} years old`;
 };
 
-// A sqaure type would have additional properties
-type Square = {
-  kind: ShapeKind.Square;
-  sideLength: number;
+// Using the arrow function syntax
+const intro3 = (name: string, age: number): string => {
+  return `My name is ${name} and I am ${age} years old`;
 };
-
-// Now enuma act as types as TS is able to identify that square enum cannot be assigned to circle type
-let circle: Circle = {
-  kind: ShapeKind.Square,
-  radius: 100,
-};
-
-//  Enums automatically become union of each of its members
-// Here the ShapeKind Enum is acting as a union of ""
-function printShape(shape: ShapeKind /* same as "Circle"| "Square"  */) {
-  // We can check teh shape in step one
-  console.log(`Shape is: ${shape}`);
-  // Also an or comparison TS will say that this is already an enum so this comparison is not needed
-  if (ShapeKind.Circle || ShapeKind.Square) {
-  }
-}
-
-// We can invoke the function
-printShape(ShapeKind.Circle);
