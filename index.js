7430295fd6659b99af4a924a90dac314ba5166b3
Manik <manik@pixelconception.com>
2024-07-08 19:23:11 +0530

    Final code for TCC121

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
 "use strict";
-// Example if function overloading
-// JavaScritp functions as Dynamic in natrue they can accept different arguments and based on that return results
-// Here is an example of the same
-// Show the slice method signature to show optional params
-const str = "Hello, World!";
-const part1 = str.slice(7);
-const part2 = str.slice(7, 10);
-// See this returns a different result and its up to you how you invoke the function
-// Whether you use single argument or two JS functions are Dynamic to return results
-// based on the arguments passed to the function
-console.log(part1);
-console.log(part2);
-const reserve = (departureDate, returnDateOrDepartingFrom, departingFromOrDestination, destination) => {
-    if (returnDateOrDepartingFrom instanceof Date && destination) {
-        return {
-            departureDate: departureDate,
-            returnDate: returnDateOrDepartingFrom,
-            departingFrom: departingFromOrDestination,
-            destination: destination,
-        };
-    }
-    else if (typeof returnDateOrDepartingFrom === "string") {
-        return {
-            departureDate: departureDate,
-            departingFrom: returnDateOrDepartingFrom,
-            destination: departingFromOrDestination,
-        };
-    }
-    throw new Error("Please provide valid details to reserve a ticket");
-};
-// Try making a reservation
-console.log(reserve(new Date(), new Date(), "New York", "Washington"));
-console.log(reserve(new Date(), "New York", "Washington"));
+// Let us look at this simple function which just returns the param
+// function returnParam(param) {
+//   return param;
+// }
+// TS has a problem and is warning that param is of any type or is implicitly declared at any
