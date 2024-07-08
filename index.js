091cd7fb02c57546e83134f5e91704e39523bd17
Manik <manik@pixelconception.com>
2024-07-08 14:38:48 +0530

    Updating code for TCC91

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
 // We have an enume for shapes
 var ShapeKind;
 (function (ShapeKind) {
-    ShapeKind[ShapeKind["Circle"] = 0] = "Circle";
-    ShapeKind[ShapeKind["Square"] = 1] = "Square";
+    ShapeKind["Circle"] = "circle";
+    ShapeKind["Square"] = "sqaure";
 })(ShapeKind || (ShapeKind = {}));
 // Now enuma act as types as TS is able to identify that square enum cannot be assigned to circle type
 let circle = {
     kind: ShapeKind.Square,
     radius: 100,
 };
+//  Enums automatically become union of each of its members
+// Here the ShapeKind Enum is acting as a union of ""
+function printShape(shape /* same as "Circle"| "Square"  */) {
+    // We can check teh shape in step one
+    console.log(`Shape is: ${shape}`);
+    // Also an or comparison TS will say that this is already an enum so this comparison is not needed
+    if (ShapeKind.Circle || ShapeKind.Square) {
+    }
+}
+// We can invoke the function
+printShape(ShapeKind.Circle);
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // We have an enume for shapes
 enum ShapeKind {
-  Circle,
-  Square,
+  Circle = "circle",
+  Square = "sqaure",
 }
 
 // A circle type would have additional properties
@@ -21,3 +21,16 @@ let circle: Circle = {
   kind: ShapeKind.Square,
   radius: 100,
 };
+
+//  Enums automatically become union of each of its members
+// Here the ShapeKind Enum is acting as a union of ""
+function printShape(shape: ShapeKind /* same as "Circle"| "Square"  */) {
+  // We can check teh shape in step one
+  console.log(`Shape is: ${shape}`);
+  // Also an or comparison TS will say that this is already an enum so this comparison is not needed
+  if (ShapeKind.Circle || ShapeKind.Square) {
+  }
+}
+
+// We can invoke the function
+printShape(ShapeKind.Circle);
