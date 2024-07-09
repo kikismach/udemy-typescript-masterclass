d0d6aab447b1ad41cd979f3550140ca793e0fbf3
Manik <manik@pixelconception.com>
2024-07-09 10:49:26 +0530

    Final code for TCC125

--- index.ts
+++ index.ts
@@ -1,43 +1,49 @@
-type KeyValuePair<K, V> = {
-  key: K;
-  value: V;
-};
-
-const stringNumberPair: KeyValuePair<string, number> = {
-  key: "age",
-  value: 30,
-};
-
-const numberBooleanPair: KeyValuePair<number, boolean> = {
-  key: 1,
-  value: true,
-};
-
-console.log(stringNumberPair); // Output: { key: 'age', value: 30 }
-console.log(numberBooleanPair); // Output: { key: 1, value: true }
-
-/**
- * Generics Constraints With Objects
- */
-
-type HasId = {
+type Events = {
   id: number;
+  date: Date;
+  type: "indoor" | "outdoor";
 };
 
-function printId<T extends HasId>(obj: T): void {
-  console.log(obj.id);
-}
+// The keyof operator takes an object type and produces a string or numeric literal union of its keys. The following type P is the same type
+// -> "id" | "date" | "type"
 
-const user = {
-  id: 1,
-  name: "Alice",
+type UnionOfKeysOfEvents = keyof Events;
+
+// You see these are literally the union of name of the keys of the Events object
+let idOfEvent: UnionOfKeysOfEvents = "id";
+let dateOfEvent: UnionOfKeysOfEvents = "date";
+
+// If index signatures where keys are defined as numeric properties
+type Numeric = {
+  [key: number]: string;
 };
 
-printId(user); // Output: 1
+type NumericKeyOf = keyof Numeric;
 
-const product = {
-  id: 101,
-  name: "Laptop",
+type NumberAndString = {
+  [key: string]: string;
+};
+// We get a union of numbers as well as a string because this is how JavaScript objects work behind the scenes
+// NumberAndString is string | number — this is because JavaScript object keys are always coerced to a string, so obj[0] is always the same as obj["0"].
+type NumberAndStringKeyoff = keyof NumberAndString;
+
+let stringObject: NumberAndString = {
+  0: "first",
+  second: "first",
 };
 
-printId(product); // Output: 101
+// Accessing the object proerty with the index of the property
+console.log(stringObject["0"]);
+
+// Declaring partial types using generics and keyof
+type Person = {
+  name: string;
+  age: number;
+  address: string;
+};
+
+// Creating a type where the keys are the same as Person but the values are optional and nullable
+// Hover over PartialPerson to see how TypeScript is inferring it
+type PartialPerson = {
+  [K in keyof Person]?: Person[K] | null;
+};
