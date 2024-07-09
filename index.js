42a6bf4335c3c6ee7b3760611ff2d8f7bacc14b4
Manik <manik@pixelconception.com>
2024-07-09 11:11:23 +0530

    Updating the branch code

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 "use strict";
-// Let us look at this simple function which just returns the param
-//* function returnParam(param) {
-//*   return param;
-//* }
-// TS has a problem and is warning that param is of any type or is implicitly declared at any
-//* function returnParam(param: any) {
-//*   return param;
-//* }
-// While using any is certainly generic in that it will cause the function to accept any and all types for the type of arg, we actually are losing the information about what that type was when the function returns. If we passed in a number, the only information we have is that any type could be returned.
-// Since now we do not know the return type of this function we end up usign the return type as
-// any as well. We are back to JS behaviour
-//* function returnParam(param: any): any {
-//*   return param;
-//* }
-// Here, we will use a type variable, a special kind of variable that works on types rather than values. This is a generic variable we can pass to our function to retain the type of the value used
-// This is how it is done in case of functions
-function returnParam(param) {
-    return param;
+// A generic array function that gets first element of every type of array
+function getFirstElement(arr) {
+    return arr[0];
 }
-// With this generic in place the value of the type used to invoke the function is retained
-// thi is one way of invoking the function
-let stringOutput = returnParam("string");
-//Here we explicitly set Type to be string as one of the arguments to the function call, denoted using the <> around the arguments rather than ().
-// The second way is also perhaps the most common. Here we use type argument inference — that is, we want the compiler to set the value of Type for us automatically based on the type of the argument we pass in:
-let stringOutput2 = returnParam("string");
-let numberOutput = returnParam(100);
-let numberArray = returnParam([1, 2, 3]);
-let objectOutput = returnParam({ name: "Mark", age: 21 });
+// We declare two different tyopes of array
+const numberArray = [1, 2, 3];
+const stringArray = ["a", "b", "c"];
+// Typescript is correctly able to infer the value that will be return by expression
+// Even though the function is the same the returned type is different based on the input value
+let stringOutput = getFirstElement(stringArray);
+let numberOutput = getFirstElement(numberArray);
+function logLength(item) {
+    console.log(item.length);
+}
+// Any array like value that has a length property on it will be accepted as an argument
+logLength(numberArray);
+logLength(stringArray);
+logLength("Any String");
+// But if used for an object it will throw an error
+logLength({ name: "John", length: 12 });
--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
+//  First declare a generics function signature
+type GetFirstElement = <T>(arr: T[]) => T;
 // A generic array function that gets first element of every type of array
-function getFirstElement<T>(arr: T[]): T {
+const getFirstElement: GetFirstElement = (arr) => {
   return arr[0];
-}
+};
 
 // We declare two different tyopes of array
 const numberArray = [1, 2, 3];
@@ -11,6 +13,18 @@ const stringArray = ["a", "b", "c"];
 // Even though the function is the same the returned type is different based on the input value
 let stringOutput = getFirstElement(stringArray);
 let numberOutput = getFirstElement(numberArray);
+
+// Where do declare generic dictates when typescript will binc a concrete type to a generic
+// what if the above function was declared with a different placement of generic
+type FirstElement<T> = (arr: T[]) => T;
+
+// Here if the egenetic type is not passed at the time of function declaration TS will throw and error
+// So now you need to tell TypeScript which types can we used with this fucntion which solves a completely different purpose from the function that has been declared above
+// Hover over the function param and you will see that TS now identifies that param will be an
+// array of strings
+const firstElement: FirstElement<string> = (arr) => {
+  return arr[0];
+};
 
 // Generics can have constraints as well
 type HasLength = {
