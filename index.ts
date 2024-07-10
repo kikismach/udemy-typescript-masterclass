322651e085b93afa03f5a4a9209404d6288bcf40
Manik <manik@pixelconception.com>
2024-07-10 14:22:22 +0530

    updating source code on branch

--- index.ts
+++ index.ts
@@ -2,3 +2,19 @@
 // This shoudl be done when you are sure that TypeScript is not able to infer the types correctly
 let firstName = <any>"Mark";
 let lastName = "Doe" as any;
+
+let user = {
+  name: "Mark",
+  email: "mark@email.com",
+};
+
+type User = {
+  name: string;
+  email: string;
+};
+
+function fetchUser() {
+  return user as User;
+}
+
+const fetchedData = fetchUser();
