0db20d4b61fb8abc8fdbafc00c2efc8a06d28195
Manik <manik@pixelconception.com>
2024-07-28 14:39:23 +0530

    Final code for TCC146

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ class User {
         return `Hello ${this.name}`;
     }
 }
-// A class can be used to create an instance of itself
-// An instance is an object that you create from a class
+// A class is used as a type by TypeScript.
+// Here user is inferred as a type of User class
+// Should match the shape of the User class and can be annotated as a User object
 const user = new User("Mark", "Mark@email.com");
 const user2 = new User("Alice", "alice@email.com");
+// If I try to assign a property that does not exist on User class
+// TS will throw an error because it understands that user is a typeof User
+user.lastName = "Doe";
+// Even assiging incorrect types will not work
+// Here trying to assign a number to a property of type string will throw an error
+user.name = 123;
 // The objects and greet methods are now dynamic in nature
 console.log(user);
 console.log(user2);
