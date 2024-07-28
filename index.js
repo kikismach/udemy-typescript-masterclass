93f8c5639795ba85b9f9bc0a828c99a3ca527973
Manik <manik@pixelconception.com>
2024-07-28 14:46:37 +0530

    Final code for TCC147

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 class User {
-    constructor(name, email) {
+    constructor(name, email, lastName) {
         this.name = name;
         this.email = email;
     }
@@ -13,14 +13,16 @@ class User {
 // Should match the shape of the User class and can be annotated as a User object
 const user = new User("Mark", "Mark@email.com");
 const user2 = new User("Alice", "alice@email.com");
+// Create a new user with an optional propery inside the class
+const user3 = new User("John", "John@email.com", "Doe");
 // If I try to assign a property that does not exist on User class
 // TS will throw an error because it understands that user is a typeof User
 user.lastName = "Doe";
 // Even assiging incorrect types will not work
 // Here trying to assign a number to a property of type string will throw an error
 user.name = 123;
+// Trying to change the email now will also throw an errro because email is now readonly
+user.email = "another@email.com";
 // The objects and greet methods are now dynamic in nature
 console.log(user);
-console.log(user2);
-console.log(user.greet());
-console.log(user2.greet());
+console.log(user3);
--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 class User {
   // This is how you define the properties of a class
   name: string;
-  email: string;
+  // making the email as readonly property
+  readonly email: string;
+  lastName?: string;
 
-  constructor(name: string, email: string) {
+  constructor(name: string, email: string, lastName?: string) {
     this.name = name;
     this.email = email;
   }
@@ -19,6 +21,9 @@ class User {
 const user: User = new User("Mark", "Mark@email.com");
 const user2: User = new User("Alice", "alice@email.com");
 
+// Create a new user with an optional propery inside the class
+const user3: User = new User("John", "John@email.com", "Doe");
+
 // If I try to assign a property that does not exist on User class
 // TS will throw an error because it understands that user is a typeof User
 user.lastName = "Doe";
@@ -27,8 +32,9 @@ user.lastName = "Doe";
 // Here trying to assign a number to a property of type string will throw an error
 user.name = 123;
 
+// Trying to change the email now will also throw an errro because email is now readonly
+user.email = "another@email.com";
+
 // The objects and greet methods are now dynamic in nature
 console.log(user);
-console.log(user2);
-console.log(user.greet());
-console.log(user2.greet());
+console.log(user3);
