4f20fdf30e63824afacba0e276246001ed8a54b5
Manik <manik@pixelconception.com>
2024-07-28 14:11:09 +0530

    Final code for TCC145

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 "use strict";
 class User {
     constructor(name, email) {
-        // This is how you define the properties of a class
-        this.name = "John";
-        this.email = "john@email.com";
-        console.log(name);
-        console.log(email);
+        this.name = name;
+        this.email = email;
     }
     greet() {
-        return `Hello John`;
+        return `Hello ${this.name}`;
     }
 }
 // A class can be used to create an instance of itself
 // An instance is an object that you create from a class
 const user = new User("Mark", "Mark@email.com");
 const user2 = new User("Alice", "alice@email.com");
-// This class generates the same object everytime
-// We will see how to solve this in the next lecture
+// The objects and greet methods are now dynamic in nature
 console.log(user);
 console.log(user2);
 console.log(user.greet());
+console.log(user2.greet());
--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 class User {
   // This is how you define the properties of a class
-  name = "John";
-  email = "john@email.com";
+  name: string;
+  email: string;
 
   constructor(name: string, email: string) {
-    console.log(name);
-    console.log(email);
+    this.name = name;
+    this.email = email;
   }
 
   greet() {
-    return `Hello John`;
+    return `Hello ${this.name}`;
   }
 }
 
@@ -18,8 +18,8 @@ class User {
 const user = new User("Mark", "Mark@email.com");
 const user2 = new User("Alice", "alice@email.com");
 
-// This class generates the same object everytime
-// We will see how to solve this in the next lecture
+// The objects and greet methods are now dynamic in nature
 console.log(user);
 console.log(user2);
 console.log(user.greet());
+console.log(user2.greet());
