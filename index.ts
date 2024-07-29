ef5c376b67f4787ef324a7abec5a7b9442bdf56d
Manik <manik@pixelconception.com>
2024-07-29 16:15:19 +0530

    TCC181

--- index.ts
+++ index.ts
@@ -1,36 +1,17 @@
-// We can declare a separate type for the constructor to get rid of the confusion
-type Constructor = new (...args: any[]) => {};
+type Holidays = {
+  date: Date;
+  reason: string;
+}[];
 
-// A mixin function that adds timestamp functionality
-function Timestamp<TBase extends Constructor>(Base: TBase) {
-  // The function returns a new class that extends the passed Base class
-  return class extends Base {
-    protected timestamp: Date = new Date();
-
-    getTimestamp() {
-      return this.timestamp;
-    }
-  };
+abstract class Department {
+  protected abstract holidays: Holidays;
+  protected constructor(protected name: string) {}
 }
 
-// Base User class
-class User {
-  constructor(public name: string) {}
+class ItDepartment extends Department {
+  protected holidays: Holidays = [];
 }
 
-// Composing the final class using mixins
-class UserWithTimestamp extends Timestamp(User) {
-  constructor(name: string, public age: number) {
-    super(name);
-  }
-
-  // Since timestamp is protected we can declare a displayInfor method to
-  // display the required information
-  displayInfo() {
-    console.log(`Name: ${this.name}, Age: ${this.age}`);
-    console.log(`Timestamp: ${this.getTimestamp()}`); // Call method from Timestamp mixin
-  }
+class AdminDepartment extends Department {
+  protected holidays: Holidays = [];
 }
-
-const user = new UserWithTimestamp("Alice", 30);
-user.displayInfo();
