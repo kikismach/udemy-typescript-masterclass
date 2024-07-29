247555df72f0a48167eac55f942a563d569523c3
Manik <manik@pixelconception.com>
2024-07-29 12:44:51 +0530

    Final code for TCC165

--- index.ts
+++ index.ts
@@ -1,21 +1,25 @@
-class Box<T> {
-  private _value: T;
+type Identifiable = {
+  id: number;
+};
 
-  constructor(value: T) {
-    this._value = value;
+// We need to declare a generic class that can hold items which have id's
+// It can be a repository of any entity that has an id property associated with it
+class Repository<T extends Identifiable> {
+  private items: T[] = [];
+
+  add(item: T): void {
+    this.items.push(item);
   }
 
-  get value(): T {
-    return this._value;
+  getById(id: number): T | undefined {
+    return this.items.find((item) => item.id === id);
   }
 
-  set value(newValue: T) {
-    this._value = newValue;
+  getAll(): T[] {
+    return this.items;
+  }
+
+  removeById(id: number): void {
+    this.items = this.items.filter((item) => item.id !== id);
   }
 }
-
-const numberBox = new Box<number>(123);
-console.log(numberBox.value); // Output: 123
-
-const stringBox = new Box<string>("Hello");
-console.log(stringBox.value); // Output: Hello
