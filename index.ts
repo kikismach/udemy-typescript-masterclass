9ef564930374f894217b303dde0c83ae545564df
Manik <manik@pixelconception.com>
2024-07-29 16:41:16 +0530

    TCC197

--- index.ts
+++ index.ts
@@ -28,37 +28,29 @@ interface Automobile<Type, Brand, Colors> {
   description: string;
 }
 
-class Car implements Automobile<string, AutomobileBrands, AutomobileColors> {
-  public type: string = "car";
-
-  constructor(
-    public brand: AutomobileBrands,
-    public colors: AutomobileColors[],
-    public description: string
-  ) {}
+interface CommercialVehicle {
+  capacity: string;
+  licenseRenewalDate: Date;
 }
 
-class Truck implements Automobile<string, AutomobileBrands, AutomobileColors> {
+class Truck implements Automobile<string, AutomobileBrands, AutomobileColors>, CommercialVehicle {
   public type: string = "truck";
 
   constructor(
     public brand: AutomobileBrands,
     public colors: AutomobileColors[],
-    public description: string
+    public description: string,
+    public capacity: string,
+    public licenseRenewalDate: Date
   ) {}
 }
-
-const ferrari: Car = new Car(
-  AutomobileBrands.ferrari,
-  [AutomobileColors.red, AutomobileColors.black],
-  "This is a Ferrari"
-);
 
 const toyotaTruck: Truck = new Truck(
   AutomobileBrands.toyota,
   [AutomobileColors.black, AutomobileColors.silver],
-  "This is a Toyota Truck"
+  "This is a Toyota Truck",
+  "15 Tonners",
+  new Date()
 );
 
-console.log(ferrari);
 console.log(toyotaTruck);
