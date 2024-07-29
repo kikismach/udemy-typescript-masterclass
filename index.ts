dcbd1f13534c9cad60d97b157dca956f3658aedf
Manik <manik@pixelconception.com>
2024-07-29 16:17:05 +0530

    TCC183

--- index.ts
+++ index.ts
@@ -3,10 +3,17 @@ type Holidays = {
   reason: string;
 }[];
 
-// Abstract class
 abstract class Department {
   protected abstract holidays: Holidays;
   protected constructor(protected name: string) {}
+
+  public addHolidays(holidays: Holidays) {
+    if (Array.isArray(holidays)) {
+      for (const holiday of holidays) {
+        this.holidays.push(holiday);
+      }
+    }
+  }
 }
 
 class ItDepartment extends Department {
