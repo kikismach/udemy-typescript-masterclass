f9335345e92c0f4f1d8f0ac8d76509936fef6087
Manik <manik@pixelconception.com>
2024-07-30 13:38:04 +0530

    Final code for TCC271

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,35 @@
-interface MapLocation {
-  lat: number;
-  long: number;
+interface StringValidator {
+  isAcceptable(key: string): boolean;
+}
+let lettersRegexp = /^[A-Za-z]+$/;
+let numberRegexp = /^[0-9]+$/;
+
+class LettersOnlyValidator implements StringValidator {
+  isAcceptable(value: string) {
+    return lettersRegexp.test(value);
+  }
 }
 
-function AddLocation(lat: number, long: number) {
-  return <T extends { new (...args: any[]): {} }>(classConstructor: T) => {
-    return class extends classConstructor {
-      public mapLocation: MapLocation;
-      constructor(...args: any[]) {
-        super(...args);
-        this.mapLocation = { lat, long };
-      }
-    };
-  };
+class ZipCodeValidator implements StringValidator {
+  isAcceptable(value: string) {
+    return value.length === 5 && numberRegexp.test(value);
+  }
 }
 
-@AddLocation(1.234, 1.876)
-class Person {
-  constructor(public name: string, public age: number) {}
-}
+// Sample values to validate
+let values = ["Hello", "98052", "101"];
 
-const person: Person = new Person("John", 32);
-console.log(person);
+// Creating new validators
+let validators: { [value: string]: StringValidator } = {};
+validators.zipCode = new ZipCodeValidator();
+validators.letters = new LettersOnlyValidator();
+
+console.log(validators);
+
+// Show whether each string passed each validator
+for (let each of values) {
+  for (let name in validators) {
+    let isMatch = validators[name].isAcceptable(each);
+    console.log(`'${each}' ${isMatch ? "matches" : "does not match"} '${name}'.`);
+  }
+}
