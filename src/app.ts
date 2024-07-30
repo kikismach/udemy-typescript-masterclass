e0f4e952548e1b1b4adae5727f590848e8d56bc6
Manik <manik@pixelconception.com>
2024-07-30 17:35:18 +0530

    Final code for TCC276

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import { add, subtract } from "./functions/math.js";
+import printHello = require("./functions/strings.js");
 
 console.log(add(2, 3)); // 5
 console.log(subtract(5, 3)); // 2
+printHello();
