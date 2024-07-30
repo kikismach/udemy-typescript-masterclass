4b4155bc21610b532fbb7fa25bfd8a4b7fc65f0d
Manik <manik@pixelconception.com>
2024-07-30 12:33:22 +0530

    TCC262

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,35 @@
-function ParameterDecorator(classPrototype: Object, methodname: string, index: number) {
+function PropertyDecorator(classPrototype: Object, propertyName: string) {
   console.log(classPrototype);
-  console.log(methodname);
-  console.log(index);
+  console.log(propertyName);
+}
+
+function AccessorDecorator(
+  classPrototype: Object,
+  accessorName: string,
+  propertyDescriptor: PropertyDescriptor
+) {
+  console.log(classPrototype);
+  console.log(accessorName);
+  console.log(propertyDescriptor);
 }
 
 class Airplane {
-  constructor(public _aircraftModel: string) {}
+  @PropertyDecorator
+  public _aircraftModel: string;
+  constructor(aircraftModel: string) {
+    this._aircraftModel = aircraftModel;
+  }
 
   public static seatCount(): void {
     console.log("150 Seats");
   }
 
-  public pilotName(name: string, @ParameterDecorator lastName: string) {
+  public pilotName(name: string, lastName: string) {
     console.log(name);
     console.log(lastName);
   }
 
+  @AccessorDecorator
   public get aircraftModel() {
     return this._aircraftModel;
   }
