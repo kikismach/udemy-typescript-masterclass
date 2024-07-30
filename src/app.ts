18d31a69fc26a3f9a80c84715d09780581b7b9fe
Manik <manik@pixelconception.com>
2024-07-30 13:48:41 +0530

    Final code for TCC272

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,49 @@
-interface StringValidator {
-  isAcceptable(key: string): boolean;
-}
-let lettersRegexp = /^[A-Za-z]+$/;
-let numberRegexp = /^[0-9]+$/;
+/** 
+ * 
+ As we add more validators, we’re going to want to have some kind of organization scheme so that we can keep track of our types and not worry about name collisions with other objects. Instead of putting lots of different names into the global namespace, let’s wrap up our objects into a namespace.
+ */
 
-class LettersOnlyValidator implements StringValidator {
-  isAcceptable(value: string) {
-    return lettersRegexp.test(value);
+// Lets add all our validation logic inside a namespace called Validation
+namespace Validation {
+  // Since interface is used outside the code we need to export it
+  export interface StringValidator {
+    isAcceptable(key: string): boolean;
+  }
+
+  // We do not need to export the constants because they are not used
+  // By out code outside of the namespace they are implementation details
+  let lettersRegexp = /^[A-Za-z]+$/;
+  let numberRegexp = /^[0-9]+$/;
+
+  // Since class is used outside the code we need to export it
+  export class LettersOnlyValidator implements StringValidator {
+    isAcceptable(value: string) {
+      return lettersRegexp.test(value);
+    }
+  }
+
+  // Since class is used outside the code we need to export it
+  export class ZipCodeValidator implements StringValidator {
+    isAcceptable(value: string) {
+      return value.length === 5 && numberRegexp.test(value);
+    }
   }
 }
 
-class ZipCodeValidator implements StringValidator {
-  isAcceptable(value: string) {
-    return value.length === 5 && numberRegexp.test(value);
-  }
-}
-
-// Sample values to validate
 let values = ["Hello", "98052", "101"];
 
-// Creating new validators
-let validators: { [value: string]: StringValidator } = {};
-validators.zipCode = new ZipCodeValidator();
-validators.letters = new LettersOnlyValidator();
+// Now wherever we need to use any of the types declare above in a namespace we need
+// to qualify them with the namespace name just like you would access the property of an object
+// using the dot notation.
+
+// Doing this we have now assigned a namespce to all our validation types and these names
+// now do no exist in the GLOBAL namespace, but are specific to Validation namespace
+let validators: { [value: string]: Validation.StringValidator } = {};
+validators.zipCode = new Validation.ZipCodeValidator();
+validators.letters = new Validation.LettersOnlyValidator();
 
 console.log(validators);
 
-// Show whether each string passed each validator
 for (let each of values) {
   for (let name in validators) {
     let isMatch = validators[name].isAcceptable(each);
