f6ac66f0e0b157d28bfee97e1f231a183b32cf49
Manik <manik@pixelconception.com>
2024-07-30 12:26:33 +0530

    TCC259

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ enum Manufacturers {
 
 interface AircraftInterface {
   _aircraftModel: string;
+  pilotName: () => void;
   prototype?: any;
   origin?: string;
   manufacturer?: string;
@@ -13,30 +14,21 @@ interface AircraftInterface {
   boeingMethod?: () => void;
 }
 
-function AircraftManufacturer(manufacturer: Manufacturers) {
-  return (target: Function) => {
-    if (manufacturer === Manufacturers.airbus) {
-      target.prototype.origin = "United States Of America";
-      target.prototype.manufacturer = Manufacturers.airbus;
-      target.prototype.type = "Jet";
-      target.prototype.airbusMethod = () => {
-        console.log("Function performed by Airbus");
-      };
-    } else {
-      target.prototype.origin = "France";
-      target.prototype.manufacturer = Manufacturers.boeing;
-      target.prototype.type = "Helicopter";
-      target.prototype.boeingMethod = () => {
-        console.log("Function performed by Boeing");
-      };
-    }
-  };
+function MethodDecorator(
+  classPrototype: Object,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log(classPrototype);
+  console.log(methodName);
+  console.log(descriptor);
+  descriptor.writable = true;
 }
 
-@AircraftManufacturer(Manufacturers.airbus)
 class Airplane implements AircraftInterface {
   constructor(public _aircraftModel: string, private pilot: string) {}
 
+  @MethodDecorator
   public pilotName() {
     console.log(this.pilot);
   }
@@ -45,25 +37,8 @@ class Airplane implements AircraftInterface {
     return this._aircraftModel;
   }
 }
-
-@AircraftManufacturer(Manufacturers.boeing)
-class Helicopter implements AircraftInterface {
-  constructor(public _aircraftModel: string, private pilot: string) {}
-
-  public pilotName() {
-    console.log(this.pilot);
-  }
-
-  public get aircraftModel() {
-    return this._aircraftModel;
-  }
-}
-
 const airplane: AircraftInterface = new Airplane("Airbus A380", "John");
 
-const helicopter: AircraftInterface = new Helicopter("H380", "Mark");
+airplane.pilotName = () => console.log("Function Changed");
 
-console.log(airplane);
-console.log(helicopter);
-
-airplane.airbusMethod ? airplane.airbusMethod() : console.log("Method Does not Exist");
+airplane.pilotName();
