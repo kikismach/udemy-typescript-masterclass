be9b2e81901cba34f6b28b381ecb52fde461083b
Manik <manik@pixelconception.com>
2024-07-31 15:08:41 +0530

    Final code for TCC204

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,20 @@
-import "reflect-metadata";
-
-// Define a decorator to specify accessible roles
-// We want this decorator to set meta-data for an object the roles metadata
-function Role(...roles: string[]) {
-  return function (target: any, propertyKey: string) {
-    Reflect.set(target, propertyKey + "_roles", roles);
-  };
+// Base definition of User interface and class
+interface IUser {
+  id: number;
+  name: string;
 }
 
-// Declare this class first
-class UserController {
-  // Delete user can only be performed by admin
-  @Role("admin")
-  deleteUser(userId: number) {
-    console.log(`Deleting user ${userId}`);
-  }
-
-  // Update user can be performed both by admin as well as user
-  @Role("admin", "user")
-  updateUser(userId: number) {
-    console.log(`Updating user ${userId}`);
-  }
+// Adding properties in the security module
+interface IUser {
+  passwordHash: string;
 }
 
-// Accessing the MetaData
-function canExecuteMethod(obj: any, methodName: string, userRole: string): boolean {
-  const roles = Reflect.get(obj, methodName + "_roles") || [];
-  console.log(Object.getOwnPropertyNames(obj));
-  return roles.includes(userRole);
+// IUser is merged automatically
+// When additional property is added in the IUser interface then TS forces you to add the new
+// property to the class for the class to completely implement the IUser interface
+class User implements IUser {
+  constructor(public id: number, public name: string) {}
 }
 
-// Create an instance of UserController
-const userController = new UserController();
-
-// Simulate method access check
-console.log(canExecuteMethod(userController, "deleteUser", "admin")); // true
-console.log(canExecuteMethod(userController, "updateUser", "guest")); // false
+let baseUser: User = new User(1, "John Doe");
+console.log(baseUser);
