7040afc9e8848d82648394604b35d2c43e7f9986
Manik <manik@pixelconception.com>
2024-07-31 13:13:26 +0530

    Final code for TCC287

--- src/calculator.d.ts
+++
@@ -1,5 +0,0 @@
-// calculator.d.ts
-export function calculate(a: number, b: number): number;
-export function calculate(a: string, b: string): string;
-export function calculate(a: number, b: string): string;
-export function calculate(a: string, b: number): string;
--- src/calculator.js
+++
@@ -1,8 +0,0 @@
-function calculate(a, b) {
-  if (typeof a === "string" || typeof b === "string") {
-    return a.toString() + b.toString();
-  }
-  return a + b;
-}
-
-export { calculate };
