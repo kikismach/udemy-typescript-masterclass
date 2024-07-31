9b4a335c8f6e607fc16c69bbead84b6886cb2f29
Manik <manik@pixelconception.com>
2024-07-31 11:54:45 +0530

    Updating code for TCC282

--- src/calculator.d.ts
+++ src/calculator.d.ts
@@ -1,1 +1,5 @@
-export function add(num1: number, num2: number): number;
+// When creating a d.ts file using the declare keyword is optional
+// Any code inside a d.ts file is a declaration
+// If this declaration was in ts file itself then you would not have to export it explicitly
+// and can use the declare keyword in that case
+export declare function add(num1: number, num2: number): number;
